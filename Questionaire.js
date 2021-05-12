f2e71e97d12ea65a1589645fb419532c2c752682
Christian <christianlm9@outlook.dk>
2021-05-12 13:28:05 +0200

    Even more changes
    The looping problem should be fixed and some of the parts have been moved to separate functions

--- Questionaire.js
+++ Questionaire.js
@@ -30,11 +30,11 @@ var Database = {
     questionGeneral: ["yeet", "jacky chan", "big chungus",
     "sanic", "pepe"],
 
-    generalValues: [[1, 3, 3], [7, 0, 0], [42, 0, 69], [6, 6, 6], [4, 2, 0]],
+    generalValues: [[1, 0, 1], [0, 1, 0], [1, 0, 1], [0, 1, 0], [1, 0, 1]],
 
     totalQuestions: 21,
 
-    totalValues:[21, 21, 21]
+    totalValues:[9, 12, 7]
 
 };
 
@@ -46,16 +46,13 @@ var storedValues = [];
 var usedQuestions = [];
 var questionAnswers = [];
 
-/* Tempoary */
-var k = 0;
-/* Tempoary */
-
 var CancerValue = [0, 0, 0];
 var COPDValue = [0, 0, 0];
 var DiabetesValue = [0, 0, 0];
 
 var i = 0;
 var j = 0;
+var k = 0;
 
 var go_to_comment = 0;
 
@@ -70,7 +67,6 @@ function firstQuestion() {
 }
 
 function next() {
-    
 
     var answer = document.Questionaire.Answer.value;
 
@@ -95,28 +91,7 @@ function next() {
         storedValues = Database.startValues;
     }
 
-    if ((COPDValue[0] > (0.9 * Database.totalValues[0])
-    && CancerValue[0] < (0.4 * Database.totalValues[1]) 
-    && DiabetesValue[0] < (0.4 * Database.totalValues[2])) 
-    || (CancerValue[0] > (0.9 * Database.totalValues[1])
-    && DiabetesValue[0] < (0.4 * Database.totalValues[2]) 
-    && COPDValue[0] < (0.4 * Database.totalValues[0])) 
-    || (DiabetesValue[0] > (0.9 * Database.totalValues[2]) 
-    && CancerValue[0] < (0.4 * Database.totalValues[1]) 
-    && COPDValue[0] < (0.4 * Database.totalValues[0]))) {
-
-        running = false;
-
-    } else if (COPDValue[0] >= Database.totalValues[0] || CancerValue[0] >= Database.totalValues[1] || DiabetesValue[0] >= Database.totalValues[2]) {
-
-        running = false;
-
-    } else if (k >= Database.totalQuestions) {
-
-        running = false;
-
-    }
-
+    stopProgram();
 
     if (running == true) {
 
@@ -135,7 +110,6 @@ function next() {
                 DiabetesValue[1] = DiabetesValue[1] + Database.firstValues[2];
         
             }
-            
             question = storedQuestions[i];
             
             document.getElementById('QUESTION').innerHTML
@@ -145,107 +119,11 @@ function next() {
 
         } else {
 
-            if (question == storedQuestions[i]) {
-
-                document.getElementById('QUESTION').innerHTML
-                    = question;
-    
-                if (answer == "yes") {
-    
-                    COPDValue[0] = COPDValue[0] + storedValues[i][0];
-                    CancerValue[0] = CancerValue[0] + storedValues[i][1];
-                    DiabetesValue[0] = DiabetesValue[0] + storedValues[i][2];
-        
-                } else if (answer == "no") {
-        
-                    COPDValue[1] = COPDValue[1] + storedValues[i][0];
-                    CancerValue[1] = CancerValue[1] + storedValues[i][1];
-                    DiabetesValue[1] = DiabetesValue[1] + storedValues[i][2];
-    
-                }
-
-                i++;
-                    
-                question = storedQuestions[i];
-                console.log(COPDValue, " ", CancerValue, " ", DiabetesValue);
-                
-                /* Tempoary */
-                for(j = 0; j <= usedQuestions.length; j++) {
-                    
-                    if (usedQuestions[j] == question) {
-                        if (i >= storedQuestions.length) break;
-                        
-                        COPDValue[1] = COPDValue[1] + storedValues[i][0];
-                        CancerValue[1] = CancerValue[1] + storedValues[i][1];
-                        DiabetesValue[1] = DiabetesValue[1] + storedValues[i][2];
-
-                        i++;
-                        if (i >= storedQuestions.length) break;
-                        question = storedQuestions[i];
-                        j=-1;
-                    
-                    }
-                    
-                }
-                
-                if(j > usedQuestions.length) {
-
-                    usedQuestions[k] = question;
-                    questionAnswers[k] = answer;
-                    k++;
-    
-                }
-                /* Tempoary */
-
-            }
+            changePage(answer);
             
         }
         
-        COPDValue[2] = COPDValue[0]-COPDValue[1];
-        CancerValue[2] = CancerValue[0]-CancerValue[1];
-        DiabetesValue[2] = DiabetesValue[0]-DiabetesValue[1];
-
-        if (i >= storedQuestions.length && COPDValue[2] > CancerValue[2] && COPDValue[2] > DiabetesValue[2]) {
-
-            storedQuestions = Database.questionsCopd;
-            storedValues = Database.copdValues;
-
-            i = 0;
-
-            question = storedQuestions[i];
-
-        } else if (i >= storedQuestions.length && CancerValue[2] > COPDValue[2] && CancerValue[2] > DiabetesValue[2]) {
-
-            storedQuestions = Database.questionsCancer;
-            storedValues = Database.cancerValues;
-
-            i = 0;
-
-            question = storedQuestions[i];
-
-        } else if (i >= storedQuestions.length && DiabetesValue[2] > CancerValue[2] && DiabetesValue[2] > COPDValue[2]) {
-            
-            storedQuestions = Database.questionsDiabetes;
-            storedValues = Database.diabetesValues;
-
-            i = 0;
-
-            question = storedQuestions[i];
-
-
-        } else if (i >= storedQuestions.length 
-                && (DiabetesValue[2] == CancerValue[2] 
-                    || DiabetesValue[2] == COPDValue[2] 
-                    || CancerValue[2] == COPDValue[2])) {
-            
-            storedQuestions = Database.questionGeneral;
-            storedValues = Database.generalValues;
-
-            i = 0;
-
-            question = storedQuestions[i];
-
-        }
+        // exchange();
 
     } else if (running == false) {
 
@@ -270,13 +148,6 @@ function next() {
         go_to_comment = 1;
 
     }
-
-    console.log(i);
-    // console.log(question);
-    // console.log(storedQuestions);
-    console.log(storedValues);
-    // console.log(usedQuestions);
-    // console.log(questionAnswers);
     
 }
 
@@ -286,16 +157,137 @@ function check() {
 
 }
 
+function changePage(answer) {
+    if (question == storedQuestions[i]) {
+        for(j = 0; j <= usedQuestions.length; j++) {
+            
+            if (usedQuestions[j] == question) {
+                if (i >= storedQuestions.length) break;
+                
+                COPDValue[1] = COPDValue[1] + storedValues[i][0];
+                CancerValue[1] = CancerValue[1] + storedValues[i][1];
+                DiabetesValue[1] = DiabetesValue[1] + storedValues[i][2];
+
+                i++;
+                if (i >= storedQuestions.length){
+                    exchange();
+                }
+                question = storedQuestions[i];
+                j=-1;
+            
+            }
+            
+        }
+
+        if(j > usedQuestions.length) {
+
+            usedQuestions[k] = question;
+            questionAnswers[k] = answer;
+            k++;
+
+            if (i < storedQuestions.length){
+                
+
+                if (answer == "yes") {
+                
+                    COPDValue[0] = COPDValue[0] + storedValues[i][0];
+                    CancerValue[0] = CancerValue[0] + storedValues[i][1];
+                    DiabetesValue[0] = DiabetesValue[0] + storedValues[i][2];
+
+                } else if (answer == "no") {
+
+                    COPDValue[1] = COPDValue[1] + storedValues[i][0];
+                    CancerValue[1] = CancerValue[1] + storedValues[i][1];
+                    DiabetesValue[1] = DiabetesValue[1] + storedValues[i][2];
+
+                }
+
+                i++;
+                exchange();
+            
+                question = storedQuestions[i];
+                console.log(COPDValue, " ", CancerValue, " ", DiabetesValue);
+
+                document.getElementById('QUESTION').innerHTML
+                    = question;
+            }
+        } 
+    }
+}
+
 // Use to exchange the questions and values for the questions instead of it being in next()
 function exchange() {
+    COPDValue[2] = COPDValue[0]-COPDValue[1];
+    CancerValue[2] = CancerValue[0]-CancerValue[1];
+    DiabetesValue[2] = DiabetesValue[0]-DiabetesValue[1];
 
+    if (i >= storedQuestions.length && COPDValue[2] > CancerValue[2] && COPDValue[2] > DiabetesValue[2]) {
+
+        storedQuestions = Database.questionsCopd;
+        storedValues = Database.copdValues;
+
+        i = 0;
+
+        question = storedQuestions[i];
+
+    } else if (i >= storedQuestions.length && CancerValue[2] > COPDValue[2] && CancerValue[2] > DiabetesValue[2]) {
+
+        storedQuestions = Database.questionsCancer;
+        storedValues = Database.cancerValues;
+
+        i = 0;
+
+        question = storedQuestions[i];
+
+    } else if (i >= storedQuestions.length && DiabetesValue[2] > CancerValue[2] && DiabetesValue[2] > COPDValue[2]) {
+        
+        storedQuestions = Database.questionsDiabetes;
+        storedValues = Database.diabetesValues;
+
+        i = 0;
+
+        question = storedQuestions[i];
+
+    } else if (i >= storedQuestions.length 
+            && (DiabetesValue[2] == CancerValue[2] 
+                || DiabetesValue[2] == COPDValue[2] 
+                || CancerValue[2] == COPDValue[2])) {
+        
+        storedQuestions = Database.questionGeneral;
+        storedValues = Database.generalValues;
+
+        i = 0;
+
+        question = storedQuestions[i];
+    }
 }
 
 function stopProgram() {
+    if ((COPDValue[0] > (0.9 * Database.totalValues[0])
+    && CancerValue[0] < (0.4 * Database.totalValues[1]) 
+    && DiabetesValue[0] < (0.4 * Database.totalValues[2])) 
+    || (CancerValue[0] > (0.9 * Database.totalValues[1])
+    && DiabetesValue[0] < (0.4 * Database.totalValues[2]) 
+    && COPDValue[0] < (0.4 * Database.totalValues[0])) 
+    || (DiabetesValue[0] > (0.9 * Database.totalValues[2]) 
+    && CancerValue[0] < (0.4 * Database.totalValues[1]) 
+    && COPDValue[0] < (0.4 * Database.totalValues[0]))) {
 
+        running = false;
+
+    } else if (COPDValue[0] >= Database.totalValues[0] || CancerValue[0] >= Database.totalValues[1] || DiabetesValue[0] >= Database.totalValues[2]) {
+
+        running = false;
+
+    } else if (k >= Database.totalQuestions) {
+
+        running = false;
+
+    }
 }
 
-function func() {
+/* This function handles the removal of the questionnaire elements and replaces them with the comment parts and submission button. */
+function commentCreate() {
     if (go_to_comment == 1){
         document.getElementById("Questionaire").remove();
 
